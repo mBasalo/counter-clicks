79c5c878a0f53d0cebe29bcb9c71badbe0e19153
Mauricio Basalo <mbasalogutierrez@gmail.com>
2023-08-13 19:58:51 -0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks (numClicks + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  }
+
   return (
 
     
@@ -16,11 +30,23 @@ function App() {
 
 
       <div className='contenedor-principal'>
+        <Contador numClicks = {numClicks} />
+
+
+        <Boton 
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick} />
+
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador} />
 
       </div>
 
     </div>
   );
-}
+} 
 
 export default App;
